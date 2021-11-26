ac735bd9a588df739ed46736fd06b505b0210a68
Daniel <danielrjoseph8@gmail.com>
2021-11-26 14:21:03 -0500

    Made changes to accelerometer

--- accelerometer/accelerometer.ino
+++ accelerometer/accelerometer.ino
@@ -1,63 +1,88 @@
-const int xInput = A0;
-const int yInput = A1;
-const int zInput = A2;
+#include "Wire.h"
+#include "LiquidCrystal.h"
 
-// initialize minimum and maximum Raw Ranges for each axis
-int RawMin = 0;
-int RawMax = 1023;
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-// Take multiple samples to reduce noise
-const int sampleSize = 10;
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+float ax=0, ay=0, az=0, gx=0, gy=0, gz=0;
 
-void setup() 
-{
-  analogReference(EXTERNAL);
-  Serial.begin(9600);
+void mpu_read(){
+ Wire.beginTransmission(MPU_addr);
+ Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+ Wire.endTransmission(false);
+ Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+ AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+ AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+ AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+ Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+ GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+ GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+ GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+ }
+
+void setup(){
+ Wire.begin();
+ Wire.beginTransmission(MPU_addr);
+ Wire.write(0x6B);  // PWR_MGMT_1 register
+ Wire.write(0);     // set to zero (wakes up the MPU-6050)
+ Wire.endTransmission(true);
+ Serial.begin(9600);
+
+ pinMode(11, OUTPUT);
+ digitalWrite(11, HIGH);
+
+  lcd.begin(16, 2);
+  // Print these words to my LCD screen
+  lcd.print("HELLO WORLD!");
+  lcd.clear();
+  
 }
+void loop(){
+  int16_t countdown = 9;
+ mpu_read();
+ //2050, 77, 1947 are values for calibration of accelerometer
+ // values may be different for you
+ ax = (AcX-2050)/16384.00;
+ ay = (AcY-77)/16384.00;
+ az = (AcZ-1947)/16384.00;
 
-void loop() 
-{
-  //Read raw values
-  int xRaw = ReadAxis(xInput);
-  int yRaw = ReadAxis(yInput);
-  int zRaw = ReadAxis(zInput);
+ //270, 351, 136 for gyroscope
+ gx = (GyX+270)/131.07;
+ gy = (GyY-351)/131.07;
+ gz = (GyZ+136)/131.07;
 
-  // Convert raw values to 'milli-Gs"
-  long xScaled = map(xRaw, RawMin, RawMax, -3000, 3000);
-  long yScaled = map(yRaw, RawMin, RawMax, -3000, 3000);
-  long zScaled = map(zRaw, RawMin, RawMax, -3000, 3000);
+ // calculating Amplitute vactor for 3 axis
+ float Raw_AM = pow(pow(ax,2)+pow(ay,2)+pow(az,2),0.5);
+ int AM = Raw_AM * 10;  // as values are within 0 to 1, I multiplied 
+                        // it by for using if else conditions 
 
-  // re-scale to fractional Gs
-  float xAccel = xScaled / 1000.0;
-  float yAccel = yScaled / 1000.0;
-  float zAccel = zScaled / 1000.0;
-
-  Serial.print("X, Y, Z  :: ");
-  Serial.print(xRaw);
-  Serial.print(", ");
-  Serial.print(yRaw);
-  Serial.print(", ");
-  Serial.print(zRaw);
-  Serial.print(" :: ");
-  Serial.print(xAccel,0);
-  Serial.print("G, ");
-  Serial.print(yAccel,0);
-  Serial.print("G, ");
-  Serial.print(zAccel,0);
-  Serial.println("G");
-
-  delay(200);
+ Serial.println(AM);
+ //Serial.println(PM);
+ //delay(500);
+ if(AM > 25){
+  while(countdown > -1){
+    if (countdown == 0) {
+      lcd.clear(); //Wipes LCD with any previous text
+      lcd.setCursor(0,0); //Positions cursor on first column and row
+      lcd.print("Elderly people");
+      lcd.setCursor(0,1); //Positions cursor on first column and second row
+      lcd.print("Fall too much!");
+      exit(0);
+  } else {
+      lcd.setCursor(0, 0); //Positions cursor on first column and row
+      lcd.print("Fall Detected, ");
+      lcd.setCursor(0, 1); //positions cursor on first column and second row
+      lcd.print("Notifying in: ");
+      lcd.setCursor(14,1); //Positions cursor on twelveth column and second row
+      lcd.print(countdown);
+      lcd.setCursor(15, 1);
+      lcd.print("s");
+      //Print current timer value on the LCD
+      countdown--; //Decrement countdown variable
+      delay(1000);
 }
-
-// Take samples and return the average
-int ReadAxis(int axisPin)
-{
-  long reading = 0;
-  analogRead(axisPin);
-  delay(1);
-  for (int i = 0; i < sampleSize; i++)
-  {
-  reading += analogRead(axisPin);
-  }
-  return reading/sampleSize;
+}
+}
 }
